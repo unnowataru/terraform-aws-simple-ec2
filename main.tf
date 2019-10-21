113b36d2fb3377980fbbcc872db2dff9ab60044f
Unno Wataru <unnow@networld.co.jp>
2019-10-21 12:46:35 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+# Region Credentials
+variable "symphony_ip" {}
+variable "secret_key" {}
+variable "access_key" {}
+variable "ami_image" {}
+variable "instance_number" {
+  default = 1
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+# Main Section
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
