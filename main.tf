4fc59c7e35d1241704b4ab3fed4f01dc9be0d2ef
Unno Wataru <unnow@networld.co.jp>
2019-10-21 11:59:12 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
-variable "aws_access_key" {}  
-variable "aws_secret_key" {}  
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
 
-provider "aws" {  
-  access_key = var.aws_access_key  
-  secret_key = var.aws_secret_key  
-  region = "ap-northeast-1"  
-}  
+  endpoints {
+    ec2 = "https://${var.symphony_ip}/api/v2/aws/ec2"
+  }
 
-resource "aws_vpc" "test-vpc" {  
-    cidr_block = "10.0.0.0/16"  
-    tags = {  
-        Name = "test-vpc"  
-    }  
-}  
+  insecure                    = true
+  skip_metadata_api_check     = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+
+  # No importance for this value currently
+  region = "us-east-1"
+}
